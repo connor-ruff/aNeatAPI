ba656539d2e456b94ee3e2518ead089ea2192071
= <connorruff99@gmail.com>
2021-01-21 12:50:13 -0500

    lifelist GET

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_cors import CORS
 import pyodbc
+import json
+import re
 
 
 app = Flask(__name__)
@@ -20,7 +22,41 @@ def home():
         output = str(row)
     except Exception as e:
         output = 'FAILURE' + str(e)
+
     return output
+
+@app.route('/lifelist')
+def lifeList():
+
+    output = {}
+
+    try:
+        output['Result'] = 'Success'
+        
+
+        conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};Server=tcp:ruffserver.database.windows.net,1433;Database=funData;Uid=connorruff;Pwd=Charlotte99!;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
+        cursor = conn.cursor()
+        cursor.execute('SELECT * from dbo.LifeList')
+
+        output['Data'] = {}
+
+        for row in cursor.fetchall():
+            birdObj = {}
+            birdObj['Species'] = row[1]
+            birdObj['Category'] = row[2]
+            birdObj['CategoryAlt'] = row[3]
+            birdObj['FirstSightCity'] = row[4]
+            birdObj['FirstSightState'] = row[5]
+            birdObj['FirstSightDetails'] = row[6]
+            birdObj['FirstSightDate'] = str(row[7])
+
+            output['Data'][row[0]] = birdObj
+
+    except Exception as e:
+        output['Result'] = 'Failure'
+        output['Message'] = str(e)
+
+    return json.dumps(output)
 
 
 if __name__ == '__main__':
