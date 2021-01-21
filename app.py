b1ec61d4a71ac29d2ebf9e2c68b613cbd646b630
= <connorruff99@gmail.com>
2021-01-21 12:14:01 -0500

    naively trying again

--- app.py
+++ app.py
@@ -10,13 +10,16 @@ app.config["DEBUG"] = True
 
 @app.route('/')
 def home():
-    conn = pyodbc.connect('Driver={ODBC Driver 13 for SQL Server};Server=ruffserver.database.windows.net;Database=funData;Uid=connorruff;Pwd=Charlotte99!')
+    try:
+        conn = pyodbc.connect('Driver={ODBC Driver 13 for SQL Server};Server=tcp:ruffserver.database.windows.net,1433;Database=funData;Uid=connorruff;Pwd=Charlotte99!;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
 
-    cursor = conn.cursor()
-    cursor.execute('SELECT * from dbo.YearList2021 where BIRD_ID = 1')
+        cursor = conn.cursor()
+        cursor.execute('SELECT * from dbo.YearList2021 where BIRD_ID = 1')
 
-    row = cursor.fetchone()
-    output = str(row)
+        row = cursor.fetchone()
+        output = str(row)
+    except Exception as e:
+        output = 'FAILURE' + str(e)
     return output
 
 
