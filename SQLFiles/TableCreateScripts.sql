dcd1dc565f5d7db7a8c071ed9e97be599d28669d
= <connorruff99@gmail.com>
2021-01-20 19:53:47 -0500

    first commit

---
+++ SQLFiles/TableCreateScripts.sql
@@ -0,0 +1,36 @@
+CREATE TABLE LifeList(
+    BIRD_ID int IDENTITY(1,1) PRIMARY KEY,
+    SPECIES varchar(50) NOT NULL,
+    Category varchar(50) NULL,
+    CategoryAlt varchar(50) NULL,
+    First_Sight_City varchar(30) NULL,
+    First_Sight_State varchar(3) NULL,
+    First_Sight_Details varchar(50) NULL,
+    First_Sight_Date date NULL
+)
+
+CREATE TABLE YearList2021 (
+    BIRD_ID int NOT NULL,
+    SPECIES varchar(50) NOT NULL,
+    Category varchar(50) NULL,
+    CategoryAlt varchar(50) NULL,
+    First_Sight_City varchar(30) NULL,
+    First_Sight_State varchar(3) NULL,
+    First_Sight_Details varchar(50) NULL,
+    First_Sight_Date date NULL,
+    WasLifeBird BIT NOT NULL,
+)
+
+CREATE TABLE CaliList2021 (
+    BIRD_ID int NOT NULL,
+    SPECIES varchar(50) NOT NULL,
+    Category varchar(50) NULL,
+    CategoryAlt varchar(50) NULL,
+    Sight_Details varchar(50) NULL,
+    Sight_Date date NULL
+)
+
+
+select * from dbo.CaliList2021
+ 
+
