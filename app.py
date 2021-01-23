e81ce924af4b3f7410e4e07357ceab93c8082205
= <connorruff99@gmail.com>
2021-01-23 18:02:36 -0500

    year list api adddition

--- app.py
+++ app.py
@@ -25,6 +25,38 @@ def home():
 
     return output
 
+@app.route('/list2021')
+def list2021():
+    output = {}
+    try:
+        output['Result'] = 'Success'
+        conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};Server=tcp:ruffserver.database.windows.net,1433;Database=funData;Uid=connorruff;Pwd=Charlotte99!;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
+        cursor = conn.cursor()
+        cursor.execute('SELECT * from dbo.YearList2021')
+
+        output['Data'] = []
+
+        for row in cursor.fetchall():
+            birdObj = {}
+            birdObj["ID"] = row[0]
+            birdObj["Species"] =row[1]
+            birdObj["Category"] = row[2]
+            birdObj["CategoryAlt"] = row[3]
+            birdObj["FirstSightCity"] = row[4]
+            birdObj["FirstSightState"] = row[5]
+            birdObj["FirstSightDetails"] = row[6]
+            birdObj["FirstSightDate"] = str(row[7])
+            birdObj["WasLifeBird"] = row[8]
+            output['Data'].append(birdObj)
+
+    except Exception as e:
+        output['Result'] = 'Failure'
+        output['Message'] = str(e)
+
+    return json.dumps(output)     
+
+        
+
 @app.route('/lifelist')
 def listList():
     output = {}
